a290868d18503928396cb04f0e48277e604c0a82
Daniel Tari <taridepaco>
2020-10-01 16:27:21 +0200

    Initial commit

---
+++ sudoku_main.py
@@ -0,0 +1,32 @@
+'''
+Sudoku solver and generator
+
+'''
+# import numpy as np
+import random as random
+
+
+def sdku_gen():
+
+
+    return [[0,0,0],[0,0,0],[0,0,0]]
+
+
+def sdku_drain(s):
+
+    return s
+
+
+def sdku_solver(s):
+
+    return s
+
+
+def main():
+    S=sdku_gen()
+    Sd=sdku_drain(S)
+    print(Sd)
+
+
+if __name__ == "__main__":
+    main()
